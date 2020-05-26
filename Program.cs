7aeb65ab86c0551106533b90d32062e80d50e62f
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-05-26 19:23:42 -0400

    Add files via upload
    Crear un programa que pida al usuario dos números enteros y diga si el primero
    es múltiplo del segundo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int cociente;
+            int resul;
+
+            Console.WriteLine("Ingresar Numero 1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar Numero 2");
+            num2 = int.Parse(Console.ReadLine());
+
+            cociente = (num2 / num1) * 2;
+            resul = num2 - cociente;
+            if(resul == 0)
+            {
+                Console.WriteLine(num1+" Es multiplo de "+num2);
+            }
+            else
+            {
+                Console.WriteLine("No hay multiplo");
+            }
+        }
+    }
+}
