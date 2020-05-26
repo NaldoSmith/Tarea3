2f2eb176c8c1329882e2b49cee4bd283c3827591
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-05-26 19:30:57 -0400

    Add files via upload
    Crear un programa que pida al usuario dos números enteros. Si el segundo no
    es cero, mostrará el resultado de dividir entre el primero y el segundo. Por el
    contrario, si el segundo número es cero, escribirá &quot;Error: No se puede dividir
    entre cero&quot;.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int div;
+
+            Console.WriteLine("Ingresar valor 1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar valor 2");
+            num2 = int.Parse(Console.ReadLine());
+            
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: no se puede dividir entre "+num2);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else
+            {
+                div = num1 / num2;
+                Console.WriteLine(num1+"/"+num2+" = "+div);
+            }
+            
+        }
+    }
+}
