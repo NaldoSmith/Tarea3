bb506dbe62581eb588cd7b12fe0536daea81659c
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-05-26 19:28:02 -0400

    Add files via upload
    Crear un programa que multiplique dos números enteros de la siguiente forma:
    pedirá al usuario un primer número entero. Si el número que se que teclee es 0,
    escribirá en pantalla &quot;El producto de 0 por cualquier número es 0&quot;. Si se ha
    tecleado un número distinto de cero, se pedirá al usuario un segundo número y
    se mostrará el producto de ambos.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int resul;
+            Console.WriteLine("Ingresar Valor 1");
+            num1 = int.Parse(Console.ReadLine());
+            if (num1 == 0)
+            {
+                Console.WriteLine("El producto de 0 por cualquier numero es 0");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Ingresar Valor 2");
+                num2 = int.Parse(Console.ReadLine());
+                resul=num1* num2;
+                Console.WriteLine("El producto de " + num1 + " y " + num2 + " es " + resul);
+            }
+        }
+    }
+}
