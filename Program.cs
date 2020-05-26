c366ef9100339e66ad6e2c68d275a9c055f36f98
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-05-26 19:21:54 -0400

    Add files via upload
    Crear un programa que pida al usuario dos números enteros y diga cuál es el
    mayor de ellos

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            Console.WriteLine("Ingresar Valor 1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar Valor 2");
+            num2 = int.Parse(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("Ambos numeros son iguales");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1 + " Es el mayor de ambos numeros");
+            }
+            else
+            {
+                Console.WriteLine(num2 + " Es el mayor de ambos numeros");
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
