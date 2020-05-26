5eb69fca11e95eb5e8fa298003b9d9716b7b12f2
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-05-26 19:34:44 -0400

    Add files via upload
    Crear un programa que pida al usuario dos números enteros y diga &quot;Uno de los
    números es positivo&quot;, &quot;Los dos números son positivos&quot; o bien &quot;Ninguno de los
    números es positivo&quot;, según corresponda.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("Ingresar Valor 1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar Valor 2");
+            num2 = int.Parse(Console.ReadLine());
+            if (num1 > 0 && num2 > 0)
+            {
+                Console.WriteLine("Ambos numeros son positivo");
+
+            }
+            else if(num1 < 0 && num2 < 0)
+            {
+                Console.WriteLine("Ambos numeros son negativos");
+            }
+            else if(num1 > 0 && num2 < 0)
+            {
+                Console.WriteLine(num1 + " Es positivo y " + num2 + " Es negativo");
+            }
+            else if(num1<0 && num2 > 0)
+            {
+                Console.WriteLine(num1 + " Es negativo y " + num2 + " Es positivo");
+            }
+        }
+    }
+}
