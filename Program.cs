e28a010561916b2101bf864ec93921bbbfb71e01
NaldoSmith <61521053+NaldoSmith@users.noreply.github.com>
2020-05-26 19:24:40 -0400

    Add files via upload
    Crear un programa que pida al usuario un número entero. Si es múltiplo de 10,
    se lo avisará al usuario y pedirá un segundo número, para decir a continuación
    si este segundo número también es múltiplo de 10.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num = 4;
+            int cociente;
+            int resul;
+
+
+            Console.WriteLine("Ingresar valor");
+            num1 = int.Parse(Console.ReadLine());
+            cociente = (num / num1) * 2;
+            resul = num - cociente;
+            if (resul == 0)
+            {
+                Console.WriteLine(num1 + " Es Multiplo de 10");
+                Console.WriteLine();
+                Console.WriteLine("Ingresar 2do. valor");
+                num2 = int.Parse(Console.ReadLine());
+                cociente = (num / num2) * 2;
+                resul = num - cociente;
+                if (resul == 0)
+                {
+                    Console.WriteLine(num2 + " Es Multiplo de 10");
+                }
+                else
+                {
+                    Console.WriteLine(num2 + " No es Multiplo de 10");
+                }
+            }
+            else
+            {
+                Console.WriteLine(num1 + " No es Multiplo de 10");
+            }
+            Console.ReadKey();
+        }
+    }
+}
